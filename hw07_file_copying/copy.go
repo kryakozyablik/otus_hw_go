ca6590038f7b043c726b5af90cb5ffdd6614b5fc
vladimir rodionov <vrodionov@wakeapp.ru>
2020-09-15 23:01:04 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,99 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strings"
 )
 
 var (
+	ErrFileDoesNotExists     = errors.New("file does not exists")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	info, err := os.Stat(fromPath)
+	if os.IsNotExist(err) || info.IsDir() {
+		return ErrFileDoesNotExists
+	}
+
+	if info.Size() == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset > info.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	fFrom, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+
+	fTo, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	if limit == 0 {
+		limit = info.Size()
+	}
+
+	_, err = fFrom.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	copySize := info.Size() - offset
+
+	if limit < copySize {
+		copySize = limit
+	}
+
+	pReader := &progressReader{R: fFrom, CopySize: copySize}
+
+	_, err = io.CopyN(fTo, pReader, limit)
+	if err != nil && err != io.EOF {
+		return err
+	}
+
 	return nil
+}
+
+type progressReader struct {
+	R        io.Reader
+	CopySize int64
+	readed   int64
+}
+
+func (pr *progressReader) Read(p []byte) (int, error) {
+	n, err := pr.R.Read(p)
+
+	if err == nil {
+		pr.printProgress(n)
+	}
+
+	return n, err
+}
+
+func (pr *progressReader) printProgress(rs int) {
+	pr.readed += int64(rs)
+	percent := int(math.Round(float64(pr.readed) * 100 / float64(pr.CopySize)))
+
+	bar := fmt.Sprintf(
+		"\r|%s%s%s| %d%%",
+		strings.Repeat("=", percent),
+		">",
+		strings.Repeat("-", 100-percent),
+		percent,
+	)
+
+	fmt.Print(bar)
+
+	if pr.readed == pr.CopySize {
+		fmt.Println()
+	}
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,94 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type test struct {
+	offset int64
+	limit  int64
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	tmpFile, err := ioutil.TempFile("/tmp", "output")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		err := os.Remove(tmpFile.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	t.Run("file copy test", func(t *testing.T) {
+		for _, tst := range [...]test{
+			{
+				0,
+				0,
+			},
+			{
+				10,
+				20,
+			},
+			{
+				0,
+				6603,
+			},
+			{
+				6603,
+				0,
+			},
+		} {
+
+			err := Copy("testdata/input.txt", tmpFile.Name(), tst.offset, tst.limit)
+			require.Nil(t, err)
+
+			originalFile, err := ioutil.ReadFile("testdata/input.txt")
+			require.Nil(t, err)
+
+			testData := originalFile[tst.offset:]
+			if tst.limit > 0 {
+				testData = testData[:tst.limit]
+			}
+
+			fileCopy, err := ioutil.ReadFile(tmpFile.Name())
+			require.Nil(t, err)
+
+			require.Equal(t, testData, fileCopy)
+		}
+	})
+
+	t.Run("copy stream", func(t *testing.T) {
+		err := Copy("/dev/urandom", tmpFile.Name(), 0, 0)
+		require.Equal(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("file does not exists", func(t *testing.T) {
+		err := Copy("/not/exists", tmpFile.Name(), 0, 0)
+		require.Equal(t, err, ErrFileDoesNotExists)
+	})
+
+	t.Run("offset out of range", func(t *testing.T) {
+		err := Copy("testdata/out_offset0_limit10.txt", tmpFile.Name(), 10000, 0)
+		require.Equal(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("limit out of range", func(t *testing.T) {
+		err := Copy("testdata/input.txt", tmpFile.Name(), 0, 100000)
+
+		originalFile, err := ioutil.ReadFile("testdata/input.txt")
+		require.Nil(t, err)
+
+		fileCopy, err := ioutil.ReadFile(tmpFile.Name())
+		require.Nil(t, err)
+
+		require.Equal(t, originalFile, fileCopy)
+	})
 }
